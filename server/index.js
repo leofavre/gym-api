6276323fff353e69ab440b3c636450856af574fa
leofavre <contato@leofavre.com>
2019-02-16 23:29:44 -0200

    Remove console.log

--- server/index.js
+++ server/index.js
@@ -3,5 +3,3 @@ const { DATABASE_URL } = require('./constants.js');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(`${DATABASE_URL}/GymApp`, { useNewUrlParser: true });
-
-console.log('connected');
