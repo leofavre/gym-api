b70cd1abbdde5be2f92f0923eaad1e6fda7a5937
leofavre <contato@leofavre.com>
2019-02-16 23:53:15 -0300

    Change file name to plural

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('./db/connect.js');
 const app = require('./app/setup.js');
 const { PORT } = require('./constants.js');
 
-require('./resources/Training/route.js')(app);
+require('./resources/Training/routes.js')(app);
 
 app.listen(PORT, () => {
   console.log(`Started on port ${PORT}`);
