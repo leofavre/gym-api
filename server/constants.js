c0d7ab9096f9fe712dca7eeca697b93348fdaa5c
leofavre <contato@leofavre.com>
2019-02-17 11:16:01 -0300

    Parameterize Mongo DB

--- server/constants.js
+++ server/constants.js
@@ -1,3 +1,4 @@
 module.exports = {
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/GymApp',
   PORT: process.env.PORT || 3456
 };
--- server/db/connect.js
+++ server/db/connect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const { MONGO_DB_URI } = require('../constants.js');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb://localhost:27017/GymApp`, { useNewUrlParser: true });
+mongoose.connect(`${MONGO_DB_URI}`, { useNewUrlParser: true });
 
 module.exports = { mongoose };
